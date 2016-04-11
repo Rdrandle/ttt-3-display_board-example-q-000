1de1ecf70a46ffdcf02273d1719b6f1909b92023
Rdrandle <rdrandle.rr@gmail.com>
2016-04-11 21:12:59 +0000

    nitrous-backup at Mon Apr 11 21:12:59 UTC 2016

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,15 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+board =["  ","   ","   ","   ","  ","   ","   ","   ","   "]
+
+def display_board(name)
+ puts"         |   |
+      -----------
+         |   |
+      -----------
+         |   |
+#{name[x]}"
+
+end
+
+display_board(board)
+
